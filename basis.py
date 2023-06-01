71b8f00be45e6934fcc832827a8ee9a4b327de1f
Calder Sheagren <caldersheagren@gmail.com>
2023-06-01 11:17:42 -0400

    add multiprocessing

--- basis.py
+++ basis.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 ###############################################################
-# TEMPORAL BASIS ESTIMATION
-# script functionality
+# SPATIOTEMPORAL BASIS ESTIMATION
+# functions to estimate spatial and temporal basis
 #
 # Calder Sheagren
 # University of Toronto
@@ -13,14 +13,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+from multiprocessing import Pool
 
 import os
 import sys
 from sklearn import linear_model
 
 # pip install mat73
+# todo add h5py import from CMRxRecon github
 import mat73
-import cfl
+from scipy.io import loadmat, savemat
+# import cfl
 
 
 def ifft(X):
@@ -47,36 +50,47 @@ def spatial_basis(X_under, TB):
     rcn = ifft(X_under)
     shx, shy, shz, sht = rcn.shape
     basis = np.zeros_like(rcn)
-    for x in range(shx):
-        for y in range(shy):
-            for z in range(shz):
-                print(f"Progress: {round(x / shx * 100, 0)}%", end="\r")
-                basis[x, y, z, :] = fit_basis(rcn[x, y, z, :], TB[z])
+    args = [(x, y, z, rcn, TB) for x in range(shx) for y in range(shy) for z in range(shz)]
+    with Pool(5) as p:
+        results = p.starmap(fit_basis_at_index, args)
+    for r in results:
+        x, y, z, B = r
+        basis[x, y, z, :] = B
 
     return basis
 
-def fit_basis(X, T):
+def fit_basis_at_index(x, y, z, rcn, TB):
+    print(x, y, z, end="\r"
+    X = rcn[x, y, z, :]
+    T = TB[z]
     real_lr = linear_model.LinearRegression()
     real_lr.fit(np.real(T), np.real(X))
     imag_lr = linear_model.LinearRegression()
     imag_lr.fit(np.imag(T), np.imag(X))
-    return real_lr.coef_ + 1j * imag_lr.coef_
+    return (x, y, z, real_lr.coef_ + 1j * imag_lr.coef_)
+
+def write_basis(spatial_basis=None, temporal_basis=None, path=None):
+    # TODO figure out how to write mat73 files
+    out = {}
+    out["spatial_basis"] = spatial_basis 
+    out["temporal_basis"] = temporal_basis
+    savemat("spatiotemporal_basis", out)
+    return True
+  
+
     
-
-
-
 if __name__ == '__main__':
     fully_sampled_path = "/hdd/Data/CMRxRecon/SingleCoil/Mapping/TrainingSet/FullSample/P001/T1map.mat"
     data = mat73.loadmat(fully_sampled_path)['kspace_single_full']
     fft_recon = ifft(data)
-    cfl.writecfl("fft_recon", fft_recon)
+    # cfl.writecfl("fft_recon", fft_recon)
 
     under_sampled_path = "/hdd/Data/CMRxRecon/SingleCoil/Mapping/TrainingSet/AccFactor04/P001/T1map.mat"
     under_data = mat73.loadmat(under_sampled_path)['kspace_single_sub04']
     TB = temporal_basis(under_data)
 
     SB = spatial_basis(under_data, TB)
-    cfl.writecfl("spatial_basis", SB)
+    # cfl.writecfl("spatial_basis", SB)
 
 
         

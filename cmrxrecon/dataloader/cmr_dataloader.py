7162378de09c54cb2ef232dfc4a090ccfbecfc1e
Brenden <brenden.kadota@gmail.com>
2023-06-19 11:20:37 -0400

    Added inital data loader (no spatial basis yet)

--- cmrxrecon/dataloader/cmr_dataloader.py
+++ cmrxrecon/dataloader/cmr_dataloader.py
@@ -1,16 +1,42 @@
 import os
+import h5py
+from collections import namedtuple
+
 
 import torch
+import numpy as np
 from torch.utils.data import Dataset
+
+class CMR_RawSample(namedtuple):
+    fname: os.Path
+    slice_index: int    
 
 class CMR_Dataloader(Dataset):
     def __init__(self, directory, transforms = None):
         super().__init__()
+        directory = os.path.join(directory, '') # add trailing slash if not there
         self.data_files = os.listdir(directory)
         self.transforms = transforms
+
+        self.raw_data = []
+        for file in self.data_files:
+            t1_map_file = os.path.join(directory, file, 'T1map.mat')
+            with h5py.File(t1_map_file) as f:
+                # file data dimensions reversed when reading using h5py
+                file_data = f['kspace_single_full']
+                ti, kz, ky, kx = file_data.shape
+                for kz_slice in range(kz):
+                    self.raw_data.append(CMR_RawSample(t1_map_file, kz_slice))
     
     def __getitem__(self, index):
-        pass
+        file_name, slice_index = self.raw_data[index]
+        with h5py.File(file_name) as f:
+            file_data = f['kspace_single_full']
+            slice_data = file_data[:, slice_index, :, :]
 
     def __len__(self):
-        return len(self.data_files)
+        return len(self.raw_data)
+
+
+if __name__ == '__main__':
+    CMR_Dataloader('/home/kadotab/projects/def-mchiew/kadotab/SingleCoil/Mapping/TrainingSet/FullSample')
--- cmrxrecon/testing/dataloader/test_cmr_dataloader.py
+++
@@ -1,4 +0,0 @@
-from cmrxrecon.dataloader.cmr_dataloader import CMR_Dataloader
-
-def test_data_load():
-    CMR_Dataloader('/home/kadotab/projects/def-mchiew/kadotab/SingleCoil/Mapping/TrainingSet/')
